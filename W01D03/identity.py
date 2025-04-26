9d14ba016ce938d9cffa211cd1c5e278402e9526
Kunal <30358707+kunalp78@users.noreply.github.com>
2025-04-26 21:37:49 +0530

    Add files via upload

---
+++ W01D03/identity.py
@@ -0,0 +1,28 @@
+# a = "spurthy"
+# b = "spurthy"
+
+# compares the values
+# print(a == b)
+# compare the memory
+# print(id(a))
+# print(id(b))
+
+# print(a is b)
+
+# b = "kunal"
+# print(a, b)
+
+# b = a
+
+# print(a, b)
+
+l1 = [1, 2]
+# l2 = [1, 2]
+l2 = l1
+print(l1 == l2)
+
+print(id(l1))
+print(id(l2))
+
+# list is mutable or changeable
+print(l1 is l2)
